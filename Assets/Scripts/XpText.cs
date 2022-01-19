25fd314b50553fa3a96fb4eb685db39f0a8d56eb
LisaOlsson2 <lisa.olsson@elev.ga.lbs.se>
2022-01-19 12:48:51 +0100

    s

---
+++ Assets/Scripts/XpText.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class XpText : MonoBehaviour
+{
+    public Text xpText;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xpText.text = "Level" + GameObject.Find("Enemy").GetComponent<LevelSystem>().level.ToString() + " " + GameObject.Find("Enemy").GetComponent<LevelSystem>().xp.ToString() + "/" + GameObject.Find("Enemy").GetComponent<LevelSystem>().levelUp.ToString() +"xp";
+    }
+}
